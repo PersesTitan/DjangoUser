6bd2912228fa606cbefd1dcd84505ee82888c0f5
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-09-29 17:57:25 +0900

    add get_memeber create post add member

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
 import json
 
 from django.http import JsonResponse, HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
 from blog.models import Blog
+from user.models import Member
+from user.views import ID_REPOSITORY
+
+
+# 쿠키에 저장된 멤버 가져오기
+def get_member(request):
+    user_id = ID_REPOSITORY.get(request.COOKIES.get('id'))
+    if user_id is None:
+        return redirect("login")
+    return get_object_or_404(Member, pk=user_id)
 
 
 # Create your views here.
@@ -15,15 +25,22 @@ def make_board(board):
     return {"title": board.title, "content": board.content, "vis": board.vis}
 
 
+# blog-create
 @method_decorator(csrf_exempt, name='dispatch')
 class Create(View):
     def post(self, request):
+        # user.views 에 존재하는 ID_REPOSITORY 불러옴
+        member = get_member(request)
+        if type(member) != Member:
+            return redirect("login")
+
         data = json.loads(request.body)
         title = data["title"]
         content = data["content"]
         Blog(
             title=title,
-            content=content
+            content=content,
+            member=member
         ).save()
         return JsonResponse({"title": title, "content": content}, status=201)
 
@@ -36,6 +53,7 @@ class Find(View):
 
 
 # 1개 찾기 OR 특정 게시판 편집
+# /blogs/<id>/
 @method_decorator(csrf_exempt, name='dispatch')
 class FindOne(View):
     def get(self, request, blog_id):
@@ -44,14 +62,21 @@ class FindOne(View):
 
     # 편집
     def patch(self, request, blog_id):
+        # user.views 에 존재하는 ID_REPOSITORY 불러옴
+        member = get_member(request)
+
+        # Blog 가져오기
         data = json.loads(request.body)
         title = data["title"]
         content = data["content"]
         b = Blog.objects.get(pk=blog_id)
-        b.title = title
-        b.content = content
-        b.save()
-        return JsonResponse({"title": title, "content": content}, status=200)
+        if b.member == member:
+            b.title = title
+            b.content = content
+            b.save()
+            return JsonResponse({"title": title, "content": content}, status=200)
+        else:
+            return JsonResponse({"error": "수정할 수 없는 계정 입니다."}, status=403)
 
     # 보이지 않게 처리
     def delete(self, request, blog_id):
