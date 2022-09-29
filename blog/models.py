d63a1e069c8b88a52303913f8f9c424c69402aae
PersesTitan <97427878+PersesTitan@users.noreply.github.com>
2022-09-29 14:58:13 +0900

    Change DatabaseName, add member colum

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from user.models import Member
+
+
 # from user.models import User
 
 
@@ -8,6 +11,10 @@ class Blog(models.Model):
     title = models.CharField(max_length=256, blank=True)
     content = models.TextField(blank=True)
     vis = models.BooleanField(default=True)
+    member = models.ForeignKey(Member, models.CASCADE)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        db_table = "blog"
